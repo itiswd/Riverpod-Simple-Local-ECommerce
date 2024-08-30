fa8adca1bc4ca9c46a9a711b7af9cdb91a6f7dd9
itiswd <itiswdev97@gmail.com>
2024-08-30 20:31:01 +0300

    first commit

---
+++ lib/models/product.dart
@@ -0,0 +1,12 @@
+class Product {
+  final String id;
+  final String title;
+  final int price;
+  final String image;
+  const Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.image,
+  });
+}
