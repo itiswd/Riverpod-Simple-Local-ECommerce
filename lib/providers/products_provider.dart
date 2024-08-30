4547638ef5ad4f4c026f5c81d3cdb7cfc29cf19a
itiswd <itiswdev97@gmail.com>
2024-08-30 23:24:29 +0300

    Refactor Riverpod providers and integrate Riverpod Generator for improved performance and maintainability
    - Migrated `productsProvider` and `reducedProductsProvider` from `flutter_riverpod` to `riverpod_annotation` for better code generation and performance.
    - Updated `CartScreen` to use `ConsumerStatefulWidget` and `ConsumerState` for state management, integrating `productsProvider` to display cart products dynamically.
    - Added `riverpod_generator` and `build_runner` as dev dependencies to facilitate the generation of provider code.
    - Updated `pubspec.lock` to reflect new dependencies and versions for `riverpod_annotation`, `riverpod_generator`, and other related packages.
    - Enhanced the display of products in `CartScreen` by mapping over `cartProducts` to render each product with its image, title, and price.
    - Updated `pubspec.yaml` to include `riverpod_annotation` as a dependency and added `riverpod_generator` and `build_runner` under dev_dependencies for future development.

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,12 +1,15 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_files/models/product.dart';
+part 'products_provider.g.dart';
 
 //Get all products
-final productsProvider = Provider<List<Product>>((ref) {
+@riverpod
+List<Product> products(ref) {
   return allProducts;
-});
+}
 
 //Get products with price less than 50
-final reducedProductsProvider = Provider<List<Product>>((ref) {
+@riverpod
+List<Product> reducedProducts(ref) {
   return allProducts.where((product) => product.price < 50).toList();
-});
+}
--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/providers/products_provider.dart';
 
-class CartScreen extends StatefulWidget {
+class CartScreen extends ConsumerStatefulWidget {
   const CartScreen({super.key});
 
   @override
-  State<CartScreen> createState() => _CartScreenState();
+  ConsumerState<CartScreen> createState() => _CartScreenState();
 }
 
-class _CartScreenState extends State<CartScreen> {
+class _CartScreenState extends ConsumerState<CartScreen> {
   bool showCoupon = true;
 
   @override
   Widget build(BuildContext context) {
+    final cartProducts = ref.watch(productsProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cart'),
@@ -20,10 +23,29 @@ class _CartScreenState extends State<CartScreen> {
       ),
       body: Container(
         padding: const EdgeInsets.all(30),
-        child: const Column(
+        child: Column(
           children: [
             Column(
-              children: [], // output cart products here
+              children: cartProducts.map(
+                (product) {
+                  return Container(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          width: 56,
+                          height: 56,
+                          product.image,
+                        ),
+                        const SizedBox(width: 12.0),
+                        Text(product.title),
+                        const Spacer(),
+                        Text(product.price.toString()),
+                      ],
+                    ),
+                  );
+                },
+              ).toList(), // output cart products here
             ),
 
             // output totals here
