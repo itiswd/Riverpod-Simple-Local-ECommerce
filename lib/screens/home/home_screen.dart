ce0198ff8b9788f2685a01a0eae9f6c99daf25c8
itiswd <itiswdev97@gmail.com>
2024-08-30 22:37:16 +0300

    Refactor HomeScreen to use Riverpod for state management
    Convert HomeScreen from StatelessWidget to ConsumerWidget to integrate Riverpod for state management. This change allows the screen to watch the productsProvider, dynamically updating the UI based on the current state of products. Additionally, the GridView.builder now dynamically generates grid items based on the length of the products list, enhancing the responsiveness and interactivity of the HomeScreen. Stylistic changes include adjusting the AppBar background color and enhancing the layout of grid items to include product images, titles, and prices.

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/providers/products_provider.dart';
 import 'package:riverpod_files/shared/cart_icon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final products = ref.watch(productsProvider);
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueGrey.withOpacity(0.05),
         title: const Text('Garage Sale Products'),
         actions: const [CartIcon()],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: GridView.builder(
-          itemCount: 8,
-          gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(
+          itemCount: products.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             mainAxisSpacing: 20,
             crossAxisSpacing: 20,
             childAspectRatio: 0.9,
           ),
-          itemBuilder:(context, index) {
+          itemBuilder: (context, index) {
             return Container(
               padding: const EdgeInsets.all(20),
               color: Colors.blueGrey.withOpacity(0.05),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Spacer(flex: 3),
+                  Image.asset(
+                    width: 64,
+                    height: 64,
+                    products[index].image,
+                  ),
+                  const Spacer(flex: 3),
+                  Text(products[index].title),
+                  const Spacer(flex: 2),
+                  Text(products[index].price.toString()),
+                ],
+              ),
             );
           },
         ),
